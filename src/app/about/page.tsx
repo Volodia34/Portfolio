95fba211107f72c660effbd2b132154a2a8eddc5
Volodia34 <120255295+Volodia34@users.noreply.github.com>
2025-05-16 22:19:08 +0300

    feat: add header

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,10 +1,12 @@
 import PageHeaderAbout from "@/components/about/PageHeaderAbout";
+import MyStorySection from "@/components/about/MyStorySection";
 
 
 export default function AboutPage() {
     return (
         <>
             <PageHeaderAbout />
+            <MyStorySection/>
         </>
     )
 }
