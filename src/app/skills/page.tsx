018547d88a4f723d14dc03180ed8a1c4b11f5b8c
Volodia34 <120255295+Volodia34@users.noreply.github.com>
2025-05-17 12:52:42 +0300

    feat(skills): Complete structure and components for Skills page

--- src/app/skills/page.tsx
+++ src/app/skills/page.tsx
@@ -13,6 +13,7 @@ export default function SkillsPage() {
             <AdditionalSkillsSection/>
             <LearningPhilosophySectionSkills/>
             <CurrentlyLearningSectionSkills/>
+            <CTASection/>
         </>
     );
 }
