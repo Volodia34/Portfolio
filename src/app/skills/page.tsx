77760d6ff02a7956f5a7fd369d02765b822e40e6
Volodia34 <120255295+Volodia34@users.noreply.github.com>
2025-05-17 12:34:48 +0300

    feat(skills): Add AdditionalSkillsSection with scroll animations

--- src/app/skills/page.tsx
+++ src/app/skills/page.tsx
@@ -1,11 +1,13 @@
 import SkillsCategoriesSection from "@/components/skills/SkillsCategoriesSection";
 import PageHeaderSkills from "@/components/skills/PageHeaderSkills";
+import AdditionalSkillsSection from "@/components/skills/AdditionalSkillsSection";
 
 export default function SkillsPage() {
     return (
         <>
             <PageHeaderSkills/>
             <SkillsCategoriesSection/>
+            <AdditionalSkillsSection/>
         </>
     );
 }
